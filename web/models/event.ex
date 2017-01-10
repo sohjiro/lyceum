f48d18fd78fc689886a64cd2e000b23d28b000ce
sohjiro <felipe@makingdevs.com>
2017-01-09 22:01:27 -0600

    coding #1 adding event module for handling event creation

---
+++ web/models/event.ex
@@ -0,0 +1,23 @@
+defmodule Lyceum.Event do
+  use Lyceum.Web, :model
+
+  schema "events" do
+    field :type, :string
+    field :name, :string
+    field :starting_date, Ecto.Date
+    field :campus, :string
+    field :quorum, :integer
+    field :price, :float
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:type, :name, :starting_date, :campus, :quorum, :price])
+    |> validate_required([:type, :name, :starting_date, :campus, :quorum, :price])
+  end
+end
