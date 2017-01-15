d3c722849c82feef71820eb52631c2d53c51707b
sohjiro <felipe@makingdevs.com>
2017-01-15 16:17:27 -0600

    coding #3 displaying events

--- lib/lyceum/core/event.ex
+++ lib/lyceum/core/event.ex
@@ -2,6 +2,8 @@ defmodule Lyceum.Core.Event do
 
   alias Lyceum.{Repo, Event}
 
+  def list, do: Repo.all(Event)
+
   def create(params) do
     %Event{}
     |> Event.changeset(params)
--- test/core/event_test.exs
+++ test/core/event_test.exs
@@ -4,7 +4,6 @@ defmodule Lyceum.Core.EventTest do
   alias Lyceum.Core.Event
 
   describe "Create an Event" do
-
     test "should create a new event" do
       params = %{"type" => "course",
                  "name" => "some course",
@@ -24,7 +23,14 @@ defmodule Lyceum.Core.EventTest do
       assert event.quorum == 10
       assert event.price == 1500.00
     end
+  end
 
+  describe "List events" do
+    test "should display all register of events" do
+      %Lyceum.Event{type: "type"} |> Repo.insert!
+
+      assert length(Event.list) == 1
+    end
   end
 
 end
