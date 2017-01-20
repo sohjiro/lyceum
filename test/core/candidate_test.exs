2e94a9f04f750f653b1380f9c3f3d23740c538c7
sohjiro <felipe@makingdevs.com>
2017-01-19 23:22:55 -0600

    coding #5 adding test for candidate

--- test/core/candidate_test.exs
+++ test/core/candidate_test.exs
@@ -1,28 +1,30 @@
 defmodule Lyceum.Core.CandidateTest do
   use Lyceum.ModelCase
 
+  alias Lyceum.Event
   alias Lyceum.Core.Candidate
 
   describe "Candidate flow" do
     test "should add a candidate to an event" do
+      event = %Event{type: "Course"} |> Repo.insert!
+
       params = %{
         name: "Name lastname",
         degree: "Student",
         email: "name_lastname@domain.com",
-        telephone: "1234567890"
+        telephone: "1234567890",
         observations: "This user has some observations"
-        event_id: 1
       }
 
-      {:ok, candidate} = Candidate.create(params)
+      {:ok, candidate} = Candidate.create(event.id, params)
 
       assert candidate.id
       assert candidate.name == "Name lastname"
       assert candidate.degree == "Student"
-      assert candidate.email == "name_lastname@domain"
+      assert candidate.email == "name_lastname@domain.com"
       assert candidate.telephone == "1234567890"
       assert candidate.observations == "This user has some observations"
-      assert candidate.event_id == 1
+      assert candidate.event_id == event.id
 
     end
   end
