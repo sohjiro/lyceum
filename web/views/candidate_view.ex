1026a4f5bfb2aec9d19b4c290a5439100a99c314
sohjiro <felipe@makingdevs.com>
2017-01-19 23:44:20 -0600

    coding #6 adding candidate view

---
+++ web/views/candidate_view.ex
@@ -0,0 +1,19 @@
+defmodule Lyceum.CandidateView do
+  use Lyceum.Web, :view
+
+  def render("show.json", %{candidate: candidate}) do
+    %{candidate: render_one(candidate, Lyceum.CandidateView, "candidate.json")}
+  end
+
+  def render("candidate.json", %{candidate: candidate}) do
+    %{
+      id: candidate.id,
+      name: candidate.name,
+      degree: candidate.degree,
+      email: candidate.email,
+      telephone: candidate.telephone,
+      observations: candidate.observations
+    }
+  end
+
+end
