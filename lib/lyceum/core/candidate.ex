73974b653aef186b2ee061f1cf093f05b8449a6b
sohjiro <felipe@makingdevs.com>
2017-01-21 15:34:21 -0600

    coding #8 getting info for candidate

--- lib/lyceum/core/candidate.ex
+++ lib/lyceum/core/candidate.ex
@@ -12,6 +12,13 @@ defmodule Lyceum.Core.Candidate do
     end
   end
 
+  def show_info(%{"id" => id}) do
+    case Repo.get(Candidate, id) do
+      nil -> {:error, :not_found}
+      candidate -> {:ok, candidate}
+    end
+  end
+
   def create(params) do
     Event
     |> Repo.get(params["event"])
