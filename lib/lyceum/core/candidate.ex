f86be6eb4b37c7c8dfcc1d580d175912a28e4b2d
sohjiro <felipe@makingdevs.com>
2017-01-20 23:06:01 -0600

    coding #6 adding function for listing candidates

--- lib/lyceum/core/candidate.ex
+++ lib/lyceum/core/candidate.ex
@@ -2,6 +2,16 @@ defmodule Lyceum.Core.Candidate do
   import Ecto
   alias Lyceum.{Repo, Candidate, Event}
 
+  def list_for_event(%{"event_id" => event_id}) do
+    with {:ok, event} <- Lyceum.Core.Event.show_info(%{"id" => event_id}) do
+      event
+      |> assoc(:candidates)
+      |> Repo.all
+    else
+      _ -> []
+    end
+  end
+
   def create(event_id, params) do
     Event
     |> Repo.get(event_id)
