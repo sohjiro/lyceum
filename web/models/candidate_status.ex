4db8061a6f796ce0cae630e39e83f2bdd0314edf
sohjiro <felipe@makingdevs.com>
2017-01-24 20:03:57 -0600

    coding #10 adding primary key false

--- web/models/candidate_status.ex
+++ web/models/candidate_status.ex
@@ -1,7 +1,7 @@
 defmodule Lyceum.CandidateStatus do
   use Lyceum.Web, :model
 
-  @primary_key: false
+  @primary_key false
   schema "candidates_statuses" do
     belongs_to :candidate, Lyceum.Candidate, primary_key: true
     belongs_to :status, Lyceum.Status, primary_key: true
