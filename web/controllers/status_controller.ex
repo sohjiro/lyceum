5ed6727d4bf7967233eed8bc060dd44d36d2bb1f
sohjiro <felipe@makingdevs.com>
2017-01-24 23:20:01 -0600

    coding #11 renaming file

--- web/controllers/status_controller.ex
+++ web/controllers/status_controller.ex
@@ -1,7 +1,6 @@
 defmodule Lyceum.StatusController do
   use Lyceum.Web, :controller
 
-  alias Lyceum.Core.Status
 
   def index(conn, _params) do
     render(conn, "index.json", statuses: Status.list())
