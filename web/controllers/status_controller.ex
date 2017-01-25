3cb6f8e40d13751befb1e2fbcf192f0c8f954f49
sohjiro <felipe@makingdevs.com>
2017-01-24 23:20:53 -0600

    coding #11 adding alias

--- web/controllers/status_controller.ex
+++ web/controllers/status_controller.ex
@@ -1,6 +1,7 @@
 defmodule Lyceum.StatusController do
   use Lyceum.Web, :controller
 
+  alias Lyceum.Core.Status
 
   def index(conn, _params) do
     render(conn, "index.json", statuses: Status.list())
