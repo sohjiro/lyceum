3599adab4148e5a35dd291a04ee7999425e3c14f
sohjiro <felipe@makingdevs.com>
2017-01-28 01:56:39 -0600

    coding #12 showing info for a status

--- lib/lyceum/core/status.ex
+++ lib/lyceum/core/status.ex
@@ -4,4 +4,11 @@ defmodule Lyceum.Core.Status do
 
   def list, do: Status |> Repo.all
 
+  def get(%{"id" => id}) do
+    case Repo.get(Status, id) do
+      nil -> {:error, :not_found}
+      status -> {:ok, status}
+    end
+  end
+
 end
