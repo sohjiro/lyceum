be3a00f72a156d5056ba384be3625e9297e140c7
sohjiro <felipe@makingdevs.com>
2017-01-28 01:58:15 -0600

    coding #12 rendering status info

--- web/views/status_view.ex
+++ web/views/status_view.ex
@@ -5,6 +5,10 @@ defmodule Lyceum.StatusView do
     %{statuses: render_many(statuses, Lyceum.StatusView, "status.json")}
   end
 
+  def render("show.json", %{status: status}) do
+    %{status: render_one(status, Lyceum.StatusView, "status.json")}
+  end
+
   def render("status.json", %{status: status}) do
     %{
       id: status.id,
