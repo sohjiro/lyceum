9721328d8bff959a13bffb0b9fbc78d745acfae3
sohjiro <felipe@makingdevs.com>
2017-01-30 23:20:39 -0600

    coding #18 creating campus module

---
+++ web/models/campus.ex
@@ -0,0 +1,18 @@
+defmodule Lyceum.Campus do
+  use Lyceum.Web, :model
+
+  schema "campuses" do
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name])
+    |> validate_required([:name])
+  end
+end
