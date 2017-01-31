42539e2f04705609c5a3b1eef67b5d7d13c144c5
sohjiro <felipe@makingdevs.com>
2017-01-30 22:16:37 -0600

    coding #28 rendering type elements

--- web/controllers/type_controller.ex
+++ web/controllers/type_controller.ex
@@ -1,4 +1,10 @@
-defmodule Lyceum.EventController do
+defmodule Lyceum.TypeController do
   use Lyceum.Web, :controller
+
+  alias Lyceum.{Repo, Type}
+
+  def index(conn, _params) do
+    render(conn, "index.json", types: Repo.all(Type))
+  end
 
 end
