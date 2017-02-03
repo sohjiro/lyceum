a4bd2d23451678fb5b290d403dd563ebda3e14ae
sohjiro <felipe@makingdevs.com>
2017-02-03 00:20:06 -0600

    coding #27 showing subject data

--- web/controllers/subject_controller.ex
+++ web/controllers/subject_controller.ex
@@ -1,4 +1,14 @@
 defmodule Lyceum.SubjectController do
   use Lyceum.Web, :controller
 
+  alias Lyceum.{Repo, Subject}
+
+  def index(conn, _params) do
+    render(conn, "index.json", types: Repo.all(Subject))
+  end
+
+  def show(conn, %{"id" => id}) do
+    render(conn, "show.json", type: Repo.get(Subject, id))
+  end
+
 end
