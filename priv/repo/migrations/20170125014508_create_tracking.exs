5ecb06270b8f7136b7d560bf759664faa05a3e60
sohjiro <felipe@makingdevs.com>
2017-02-05 19:08:59 -0600

    coding #31 renaming schema

--- priv/repo/migrations/20170125014508_create_tracking.exs
+++ priv/repo/migrations/20170125014508_create_tracking.exs
@@ -2,7 +2,7 @@ defmodule Lyceum.Repo.Migrations.CreateTracking do
   use Ecto.Migration
 
   def change do
-    create table(:candidates_statuses) do
+    create table(:tracking) do
       add :candidate_id, references(:candidates, on_delete: :nothing)
       add :status_id, references(:statuses, on_delete: :nothing)
       add :event_id, references(:events, on_delete: :nothing)
