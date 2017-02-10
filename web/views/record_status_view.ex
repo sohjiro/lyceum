2f8d5d077825a728cb28e80d863979f7b643ee54
sohjiro <felipe@makingdevs.com>
2017-02-10 00:39:04 -0600

    coding #32 rendering data

--- web/views/record_status_view.ex
+++ web/views/record_status_view.ex
@@ -1,12 +1,17 @@
 defmodule Lyceum.RecordStatusView do
   use Lyceum.Web, :view
 
+  def render("index.json", %{records_statuses: records_statuses}) do
+    %{recordStatuses: render_many(records_statuses, Lyceum.RecordStatusView, "record_status.json")}
+  end
+
   def render("show.json", %{record_status: record_status}) do
-    %{type: render_one(record_status, Lyceum.RecordStatusView, "record_status.json")}
+    %{recordStatus: render_one(record_status, Lyceum.RecordStatusView, "record_status.json")}
   end
 
   def render("record_status.json", %{record_status: record_status}) do
     %{
+      id: record_status.id,
       status: record_status.status_id,
       record: record_status.record_id
     }
--- web/views/record_view.ex
+++ web/views/record_view.ex
@@ -14,8 +14,9 @@ defmodule Lyceum.RecordView do
       id: record.id,
       candidate: record.candidate_id,
       event: record.event_id,
+      observations: record.observations,
       links: %{
-        "statuses": "statuses"
+        "record-status": "records_statuses"
       }
     }
   end
