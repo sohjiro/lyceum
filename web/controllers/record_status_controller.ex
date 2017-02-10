43dfbec6f2230a724eb3f9efa6d315044d0da730
sohjiro <felipe@makingdevs.com>
2017-02-10 00:38:19 -0600

    coding #32 addin index for listing record status

--- web/controllers/record_status_controller.ex
+++ web/controllers/record_status_controller.ex
@@ -3,6 +3,11 @@ defmodule Lyceum.RecordStatusController do
 
   alias Lyceum.Core.RecordStatus
 
+  def index(conn, params) do
+    IO.inspect params
+    render(conn, "show.json", records_statuses: [])
+  end
+
   def create(conn, %{"recordStatus" => params}) do
     with {:ok, record_status} <- RecordStatus.create(params) do
       render(conn, "show.json", record_status: record_status)
