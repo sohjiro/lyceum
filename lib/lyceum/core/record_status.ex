4d1f395b35c6d73570def451db39773d297ea48f
sohjiro <felipe@makingdevs.com>
2017-02-10 20:40:18 -0600

    coding #32 listing status for a record

--- lib/lyceum/core/record_status.ex
+++ lib/lyceum/core/record_status.ex
@@ -1,6 +1,13 @@
 defmodule Lyceum.Core.RecordStatus do
 
+  alias Lyceum.Core.Record
   alias Lyceum.{Repo, RecordStatus}
+
+  def list(params) do
+    with {:ok, record} <- Record.info(params) do
+      record |> Ecto.assoc(:statuses) |> Repo.all
+    end
+  end
 
   def create(%{"record" => record_id, "status" => status_id}) do
     %RecordStatus{}
