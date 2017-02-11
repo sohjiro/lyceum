15398fa2f64fa3d1aa7d283cad0558904579a65a
sohjiro <felipe@makingdevs.com>
2017-02-11 01:05:10 -0600

    coding #34 a little refactor... WIP

--- lib/lyceum/core/record_status.ex
+++ lib/lyceum/core/record_status.ex
@@ -1,27 +1,41 @@
 defmodule Lyceum.Core.RecordStatus do
-
+  import Ecto.Query, only: [order_by: 2]
   alias Lyceum.Core.Record
   alias Lyceum.{Repo, RecordStatus}
 
   def list(params) do
     with {:ok, record} <- Record.info(params) do
-      record |> Ecto.assoc(:statuses) |> Repo.all
+      record |> Ecto.assoc(:statuses) |> order_by(:inserted_at) |> Repo.all
     end
   end
 
-  def create(%{"record" => record_id, "status" => status_id}) do
-    last_record = %{"record_id" => record_id}
-                  |> list
-                  |> hd
+  def create(params) do
+    params = parse_params(params)
+    current_status = params |> list |> current_status
 
-    cond do
-      last_record.status_id == status_id && last_record.record_id == record_id ->
-        {:ok, last_record}
-      true ->
-        %RecordStatus{}
-        |> RecordStatus.changeset(%{status_id: status_id, record_id: record_id})
-        |> Repo.insert
+    case same?(current_status, params) do
+      {:same, status} -> {:ok, status}
+      {:insert, changeset} -> Repo.insert(changeset)
     end
+  end
+
+  defp parse_params(%{"record" => record_id, "status" => status_id}) do
+    %{"record_id" => record_id, "status_id" => status_id}
+  end
+
+  defp current_status([]), do: nil
+  defp current_status([current | _rest]), do: current
+
+  defp same?(%{status_id: status_id, record_id: record_id} = status, params) do
+    case status_id == params["status_id"] && record_id == params["record_id"] do
+      true -> {:same, status}
+      false -> {:insert, prepare_changeset(params)}
+    end
+  end
+  defp same?(nil, params), do: {:insert, prepare_changeset(params)}
+
+  defp prepare_changeset(params) do
+    RecordStatus.changeset(%RecordStatus{}, params)
   end
 
 end
--- test/core/record_status_test.exs
+++ test/core/record_status_test.exs
@@ -4,18 +4,18 @@ defmodule Lyceum.Core.RecordStatusTest do
   alias Lyceum.Core.RecordStatus
 
   describe "Record status flow" do
-    #test "Add status for a record" do
-    #  candidate = %Lyceum.Candidate{name: "Name lastname"} |> Repo.insert!
-    #  record = %Lyceum.Record{candidate_id: candidate.id} |> Repo.insert!
-    #  params = %{"status" => 2, "record" => record.id}
+    test "Add status for a record" do
+      candidate = %Lyceum.Candidate{name: "Name lastname"} |> Repo.insert!
+      record = %Lyceum.Record{candidate_id: candidate.id} |> Repo.insert!
+      params = %{"status" => 2, "record" => record.id}
 
-    #  {:ok, record_status} = RecordStatus.create(params)
+      {:ok, record_status} = RecordStatus.create(params)
 
-    #  assert record_status.id
-    #  assert record_status.status_id == 2
-    #  assert record_status.record_id == record.id
-    #  assert Repo.preload(record, :statuses).statuses |> length == 1
-    #end
+      assert record_status.id
+      assert record_status.status_id == 2
+      assert record_status.record_id == record.id
+      assert Repo.preload(record, :statuses).statuses |> length == 1
+    end
 
     test "Don't allow to duplicate previous register" do
       candidate = %Lyceum.Candidate{name: "Name lastname"} |> Repo.insert!
