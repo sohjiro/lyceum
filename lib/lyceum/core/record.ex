70505e5e77865f0c5d082fc1386bc5ebb212601d
sohjiro <felipe@makingdevs.com>
2017-02-18 10:44:36 -0600

    coding #35 ordering relationship data

--- lib/lyceum/core/record.ex
+++ lib/lyceum/core/record.ex
@@ -3,12 +3,18 @@ defmodule Lyceum.Core.Record do
   import Ecto.Query, only: [order_by: 2, preload: 2]
   alias Ecto.Multi
   alias Lyceum.{Repo, Record}
-  alias Lyceum.Core.Event
+  alias Lyceum.Core.{Event, Candidate}
 
   def info(%{"record_id" => id}) do
     case Repo.get(Record, id) do
       nil -> {:error, :not_found}
       record -> {:ok, Repo.preload(record, [:candidate, :statuses])}
+    end
+  end
+
+  def list(%{"candidate_id" => candidate_id}) do
+    with {:ok, candidate} <- Candidate.show_info(%{"id" => candidate_id}) do
+      candidate |> assoc(:records) |> order_by(:id) |> preload(:statuses) |> Repo.all
     end
   end
 
