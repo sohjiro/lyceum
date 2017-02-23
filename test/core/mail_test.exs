b9e5054af08e5cfae8a7af9a704f75861a36c85d
sohjiro <felipe@makingdevs.com>
2017-02-23 17:45:52 -0600

    coding #39 adding test for mail

---
+++ test/core/mail_test.exs
@@ -0,0 +1,22 @@
+defmodule Lyceum.Core.MailTest do
+  use Lyceum.ModelCase
+  import Swoosh.TestAssertions
+
+  alias Lyceum.Core.Mail
+
+  describe "Mail event flow" do
+    test "should send email for listing mails" do
+
+      params = %{"mail" =>
+                  %{"to" => "kobain@nirvana.com",
+                    "subject": "asdfasdf",
+                    "body": "<p>enjoy</p>"
+                   }
+                }
+      {:ok, sended} = Mail.send(params)
+
+      assert_email_sent [subject: "asdfasdf", to: "kobain@nirvana.com"]
+    end
+  end
+
+end
