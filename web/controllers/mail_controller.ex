0ad04bf2e99c7055b5b7cb430dddf9305e512295
sohjiro <felipe@makingdevs.com>
2017-02-24 10:31:17 -0600

    coding #39 rendering ok json... WIP

--- web/controllers/mail_controller.ex
+++ web/controllers/mail_controller.ex
@@ -1,4 +1,10 @@
 defmodule Lyceum.MailController do
   use Lyceum.Web, :controller
 
+  def create(conn, %{"mail" => mail_params}) do
+    Lyceum.Core.Mail.send_mail(mail_params)
+
+    json(conn, %{message: "Sending email"})
+  end
+
 end
