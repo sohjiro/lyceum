751988d1c19e75a8f5ec32faf6970c38aa00fffd
sohjiro <felipe@makingdevs.com>
2017-02-28 16:37:05 -0600

    coding #40 creating email data before send

--- lib/lyceum/core/mail.ex
+++ lib/lyceum/core/mail.ex
@@ -1,22 +1,33 @@
 defmodule Lyceum.Core.Mail do
   import Ecto.Query
   import Swoosh.Email, except: [from: 2]
-  alias Lyceum.{Repo, Candidate}
+  alias Lyceum.{Repo, Candidate, Mail}
   @remitent Application.get_env(:lyceum, :remitent)
   @bcc Application.get_env(:lyceum, :bcc)
 
-  def send_mail(%{"to" => to, "subject" => subject, "body" => body}) do
-    to
-    |> split
-    |> find_candidates
-    |> format_to
-    |> Enum.map(&Task.async(Lyceum.Core.Mail, :do_send_mail, [&1, subject, body]))
-    |> Enum.map(&Task.await(&1))
+  def send_mail(params) do
+    %Mail{}
+    |> Ecto.Changeset.change(bcc: Enum.join(@bcc, ","))
+    |> Mail.changeset(params)
+    |> Repo.insert
+
+
+    # to
+    # |> split
+    # |> find_candidates
+    # |> format_to
+    # |> Enum.map(&Task.async(Lyceum.Core.Mail, :do_send_mail, [&1, subject, body]))
+    # |> Enum.map(&Task.await(&1))
   end
   def do_send_mail(to, subject, body) do
     to
     |> prepare_mail(subject, body)
     |> Lyceum.Mailer.deliver
+  end
+  def save_emails(emails, _params) do
+    emails
+    |> Enum.map(fn({:ok, v}) -> v.id end)
+    |> IO.inspect
   end
 
   defp prepare_mail(to, subject, body) do
