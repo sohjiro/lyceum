039f51b6927fa3d4dde4f6c0604a16f397a30ab0
sohjiro <felipe@makingdevs.com>
2017-03-01 11:49:35 -0600

    coding #40 another approach for sending emails... WIP

--- lib/lyceum/core/mail/sender.ex
+++ lib/lyceum/core/mail/sender.ex
@@ -1,18 +1,29 @@
 defmodule Lyceum.Core.Mail.Sender do
+  use GenServer
+
   import Swoosh.Email
   @remitent Application.get_env(:lyceum, :remitent)
   @bcc Application.get_env(:lyceum, :bcc)
 
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
   def sender(mail) do
     for to <- mail.to do
-      new()
-      |> add_to_from(to)
-      |> bcc(@bcc)
-      |> from(@remitent)
-      |> subject(mail.subject)
-      |> html_body(mail.body)
-      |> Lyceum.Mailer.deliver
+      GenServer.cast(__MODULE__, {:send_mail, mail, to})
     end
+  end
+
+  def handle_cast({:send_mail, mail, to}, state) do
+    new()
+    |> add_to_from(to)
+    |> bcc(@bcc)
+    |> from(@remitent)
+    |> subject(mail.subject)
+    |> html_body(mail.body)
+    |> Lyceum.Mailer.deliver
+    {:noreply, state}
   end
 
   defp add_to_from(mail, to), do: mail |> to(format_to(to))
