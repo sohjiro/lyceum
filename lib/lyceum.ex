1c89fa0f23cc5bc274e8f982427b8917cc7c0de0
sohjiro <felipe@makingdevs.com>
2017-03-01 11:49:18 -0600

    coding #40 adding supervisor to application tree

--- lib/lyceum.ex
+++ lib/lyceum.ex
@@ -14,6 +14,7 @@ defmodule Lyceum do
       supervisor(Lyceum.Endpoint, []),
       # Start your own worker by calling: Lyceum.Worker.start_link(arg1, arg2, arg3)
       # worker(Lyceum.Worker, [arg1, arg2, arg3]),
+      supervisor(Lyceum.Core.Mail.Supervisor, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
