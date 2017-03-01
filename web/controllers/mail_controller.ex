7ea1144eca8be11c6bcfac40774d25a87b50efaf
sohjiro <felipe@makingdevs.com>
2017-03-01 12:45:05 -0600

    coding #40 adding mail view for rendering data

--- web/controllers/mail_controller.ex
+++ web/controllers/mail_controller.ex
@@ -2,9 +2,14 @@ defmodule Lyceum.MailController do
   use Lyceum.Web, :controller
 
   def create(conn, %{"mail" => mail_params}) do
-    Lyceum.Core.Mail.send_mail_flow(mail_params)
-
-    json(conn, %{message: "Sending email"})
+    with {:ok, mail} <- Lyceum.Core.Mail.send_mail_flow(mail_params) do
+      render(conn, "show.json", mail: mail)
+    else
+      _ ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{errors: %{code: "LYC-0001", message: "Bad parameters"}})
+    end
   end
 
 end
---
+++ web/views/mail_view.ex
@@ -0,0 +1,17 @@
+defmodule Lyceum.MailView do
+  use Lyceum.Web, :view
+
+  def render("show.json", %{mail: mail}) do
+    %{mail: render_one(mail, Lyceum.MailView, "mail.json")}
+  end
+
+  def render("mail.json", %{mail: mail}) do
+    %{
+      id: mail.id,
+      subject: mail.subject,
+      bcc: mail.bcc,
+      body: mail.body
+    }
+  end
+
+end
